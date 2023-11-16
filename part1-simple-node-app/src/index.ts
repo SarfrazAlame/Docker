6551596eb2e73139192d464a90c398bf2c71708f
Sarfraz Alam <sarfrazk7858@gmail.com>
2023-11-16 08:08:11 +0530

    docker

--- part1-simple-node-app/src/index.ts
+++ part1-simple-node-app/src/index.ts
@@ -7,6 +7,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/root",(req,res)=>{
+  res.json({
+    message:"Im in root route"
+  })
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
